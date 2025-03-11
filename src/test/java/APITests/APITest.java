dcfe8235af377a1100713d24302cd779ff99b6a2
Miguel Angel <miguelangelcalderon93@hotmail.com>
2025-03-11 18:33:36 -0500

    se agrega el método delete put post

--- src/test/java/APITests/APITest.java
+++ src/test/java/APITests/APITest.java
@@ -1,23 +1,65 @@
 package APITests;
 
 import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 
 public class APITest {
 
     @Test
-    public void testRickAndMortyAPI() {
+    public void testGETRickAndMortyAPI() {
         given()
-            .baseUri("https://rickandmortyapi.com/api")
+        .baseUri("https://rickandmortyapi.com/api")
             .when()
-            .get("/character/1")
+            .get()
             .then()
-            .statusCode(200)
-            .body("name", equalTo("Rick Sanchez"))
-            .body("status", equalTo("Alive"))
-            .body("species", equalTo("Human"))
-            .body("gender", equalTo("Male"));       
+            .statusCode(200);
+    }
+
+    @Test public void crearUsuario() {
+        String body = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";   
+        given()
+        .baseUri("https://reqres.in/api")
+        .contentType(ContentType.JSON)
+        .body(body)
+            .when()
+            .post("/users")
+            .then()
+            .statusCode(201)
+            .log().all();
+    }
+
+    @Test
+    public void actualizarUsuario() {
+        String body = "{\n" +
+                "    \"nombresuli\": \"Patito\",\n" +
+                "    \"chamba\": \"Free Rango\"\n" +
+                "}";
+        given()
+                .baseUri("https://reqres.in/api")
+                .contentType(ContentType.JSON)
+                .body(body)
+                .when()
+                .put("/users/690")
+                .then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    public void borrarUsuario() {
+        given()
+                .baseUri("https://reqres.in/api")
+                .when()
+                .delete("/users/690")
+                .then()
+                .statusCode(204)
+                .log().all();
     }
     
 }
